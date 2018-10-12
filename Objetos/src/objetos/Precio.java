66abd92e59459f9db7bb8679400f7bc56412b682
David <jhandry13@hotmail.com>
2018-10-12 10:22:46 -0500

    SUbir cambios al repositorio

---
+++ Objetos/src/objetos/Precio.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objetos;
+
+/**
+ *
+ * @author David Lopez
+ */
+public class Precio {
+
+    /**
+     * @param args the command line arguments
+     */
+    double euros;
+
+    public double obtener() {
+        return 0;
+    }
+
+    public void agregar(double x) {
+        
+
+    }
+}
